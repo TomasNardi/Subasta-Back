484fe5bdbc919b824fc03419fb7a3453d70cccf2
Tomas Nardi <tomas.nardi@hotmail.com>
2025-08-08 16:14:17 -0300

    feature(superuser) : new superuser create

--- subasta_app/urls.py
+++ subasta_app/urls.py
@@ -3,7 +3,8 @@ from .views import crear_subasta_api, create_superuser
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('api/subastas/crear/', crear_subasta_api, name='crear_subasta_api'),
-    path('api/create-superuser/', create_superuser, name='create_superuser'),  # <-- NUEVA RUTA
+    path('create-superuser/', create_superuser, name='create_superuser'),  # NO ES API
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
