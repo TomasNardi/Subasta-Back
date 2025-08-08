33320efbd27d7c6601d56bfaad8877d100be8c3a
Tomas Nardi <tomas.nardi@hotmail.com>
2025-08-08 15:58:06 -0300

    feature(superuser) : new superuser create

--- subasta_app/urls.py
+++ subasta_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import crear_subasta_api
+from .views import crear_subasta_api, create_superuser
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/subastas/crear/', crear_subasta_api, name='crear_subasta_api'),
+    path('api/create-superuser/', create_superuser, name='create_superuser'),  # <-- NUEVA RUTA
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- subasta_app/views.py
+++ subasta_app/views.py
@@ -4,6 +4,10 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ProductSerializer
+from django.contrib.auth.models import User
+from django.http import JsonResponse, Http404, HttpResponse
+
+
 
 @api_view(['POST'])
 @parser_classes([MultiPartParser, FormParser])  # Permite subir imagen
@@ -13,3 +17,21 @@ def crear_subasta_api(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Cree el superusuario desde el backend, con ruta por RENDER
+
+def create_superuser(request):
+    try:
+        username = "BifyAdmin"
+        password = "admin1999"
+        email = "admin@example.com"
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username, email=email, password=password
+            )
+            return HttpResponse("Superusuario creado con éxito.")
+        else:
+            return HttpResponse("El superusuario ya existe.")
+    except Exception as e:
+        return HttpResponse(f"Error: {e}")
