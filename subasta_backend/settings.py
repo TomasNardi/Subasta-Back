ef654abcc8d0290f1fc88182e0382bf7a3f96fce
Tomas Nardi <tomas.nardi@hotmail.com>
2025-08-08 13:05:33 -0300

    fix(settings) : new settings trying to update bd

--- subasta_backend/settings.py
+++ subasta_backend/settings.py
@@ -114,7 +114,7 @@ WSGI_APPLICATION = 'subasta_backend.wsgi.application'
 DATABASES = {
     "default": dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default="postgresql://postgres:postgres@localhost:5432/django_subasta",
+        default="postgresql://postgres:postgres@localhost:5432/datasubasta",
         conn_max_age=600,
     )
 }
